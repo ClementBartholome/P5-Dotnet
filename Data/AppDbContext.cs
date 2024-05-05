654c21853323bc583a02ead287ab04bd958b27a6
Clément Bartholomé <clementoss@gmail.com>
2024-05-05 13:57:41 +0200

    feat(identity): ajout d'une migration pour seed un user admin

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Express_Voitures.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,31 @@ namespace Express_Voitures.Data
         public DbSet<Finition> Finition { get; set; }
         public DbSet<VoitureVente> VoitureVente { get; set; }
         public DbSet<Annonce> Annonce { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            var hasher = new PasswordHasher<ApplicationUser>();
+            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = "1",
+                UserName = "admin@expressvoitures.com",
+                NormalizedUserName = "ADMIN",
+                Email = "admin@expressvoitures.com",
+                NormalizedEmail = "admin@expressvoitures.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Password123$"),
+                SecurityStamp = string.Empty
+            });
+            
+            modelBuilder.Entity<IdentityUserClaim<string>>().HasData(new IdentityUserClaim<string>
+            {
+                Id = 1,
+                UserId = "1",
+                ClaimType = "Admin",
+                ClaimValue = "true"
+            });
+        }
     }
 }
---
+++ Migrations/20240505115552_AddSeedData.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Express_Voitures.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixAchat",
+                table: "Voiture",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAchat",
+                table: "Voiture",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CoutReparations",
+                table: "Voiture",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Annee",
+                table: "Voiture",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "1", 0, "d78806f6-efae-4101-9e5e-f4b1963a3517", "admin@expressvoitures.com", true, false, null, "admin@expressvoitures.com", "ADMIN", "AQAAAAIAAYagAAAAEIj+gVnFpqYFPSJdudF7EOULpOPbGLrbXhYQ3M23Xl9/SfsexIGAcbX9vyCUbmPFVw==", null, false, "", false, "admin@expressvoitures.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "UserId" },
+                values: new object[] { 1, "Admin", "true", "1" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PrixAchat",
+                table: "Voiture",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAchat",
+                table: "Voiture",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CoutReparations",
+                table: "Voiture",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Annee",
+                table: "Voiture",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
